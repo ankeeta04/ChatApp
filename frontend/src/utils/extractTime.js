2c65a8c3c38c56185c9ce00e723421b79155b348
unknown <1ankeetasingh@gmail.com>
2024-11-17 20:38:17 +0530

    Deployment

---
+++ frontend/src/utils/extractTime.js
@@ -0,0 +1,16 @@
+
+
+export const getRandomEmoji = () => {
+	return funEmojis[Math.floor(Math.random() * funEmojis.length)];
+};
+export function extractTime(dateString) {
+	const date = new Date(dateString);
+	const hours = padZero(date.getHours());
+	const minutes = padZero(date.getMinutes());
+	return `${hours}:${minutes}`;
+}
+
+// Helper function to pad single-digit numbers with a leading zero
+function padZero(number) {
+	return number.toString().padStart(2, "0");
+}
